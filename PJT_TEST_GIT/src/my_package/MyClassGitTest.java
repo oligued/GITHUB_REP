b31944aa13de58f7263d5dda40fba787e076b40e
guedat <guedat@SCXGUEDATO.intranet.epfl.ch>
2012-12-10 17:08:22 +0100

    How are you ?

--- PJT_TEST_GIT/src/my_package/MyClassGitTest.java
+++ PJT_TEST_GIT/src/my_package/MyClassGitTest.java
@@ -8,7 +8,8 @@ public class MyClassGitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello !");
-
+		System.out.println("How are you ?");
+		
 	}
 
 }
